b41b03ef52b02b49861e27dcc7b96ce4311f2491
Chidhambaram <88135792+Cdbm007@users.noreply.github.com>
2022-03-27 10:06:42 +0530

    file contains the funvtion declarations

---
+++ converter.c/fun_dec.h
@@ -0,0 +1,15 @@
+float centi(float);
+float met(float);
+float kime(float);
+float mile(float);
+float naumi(float);
+float gram(float);
+float kigr(float);
+float mpou(float);
+float qui(float);
+float ton(float);
+float mill_se(float);
+float sec(float);
+float min(float);
+float hour(float);
+float day(float);
