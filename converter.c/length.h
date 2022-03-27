70ab1974927324b7c8754ef17081c298bf3ad9d1
Chidhambaram <88135792+Cdbm007@users.noreply.github.com>
2022-03-27 10:08:20 +0530

    Update length.h

--- converter.c/length.h
+++ converter.c/length.h
@@ -17,7 +17,7 @@ float centi(float er)
  switch(a)
  {
   case 1:va=er/100.0;
-	 gotoxy(20,10);printf("Centimeter : %.2f--------->Meter : ",er);
+	 gotoxy(20,25);printf("Centimeter : %.2f--------->Meter : ",er);
 	 break;
   case 2:va=er/100000.0;
 	 gotoxy(20,25);printf("Centimeter : %.2f--------->Kilometer : ",er);
@@ -66,7 +66,7 @@ float met(float er)
 float kime(float er)
 {
  clrscr();
- gotoxy(25,10);printf("Convert %f Kilometers to .......",er);
+ gotoxy(25,8);printf("Convert %f Kilometers to .......",er);
  gotoxy(20,12);printf("1.Meter...........Press 1");
  gotoxy(20,14);printf("2.Centimeter......Press 2");
  gotoxy(20,16);printf("3.Mile............Press 3");
@@ -77,16 +77,16 @@ float kime(float er)
  switch(a)
  {
   case 1:va=er*1000.0;
-	 gotoxy(20,21);printf("Kilometer : %.2f--------->Meter : ",er);
+	 gotoxy(20,25);printf("Kilometer : %.2f--------->Meter : ",er);
 	 break;
   case 2:va=er*100000.0;
-	 gotoxy(20,21);printf("Kilometer : %.2f--------->Centimeter : ",er);
+	 gotoxy(20,25);printf("Kilometer : %.2f--------->Centimeter : ",er);
 	 break;
   case 3:va=er/1.609;
-	 gotoxy(20,21);printf("Kilometer : %.2f--------->Mile : ",er);
+	 gotoxy(20,25);printf("Kilometer : %.2f--------->Mile : ",er);
 	 break;
   case 4:va=er/1.852;
-	 gotoxy(20,21);printf("Kilometer : %.2f--------->Nautical_Mile : ",er);
+	 gotoxy(20,25);printf("Kilometer : %.2f--------->Nautical_Mile : ",er);
 	 break;
  }
  return(va);
@@ -95,7 +95,7 @@ float kime(float er)
 float mile(float er)
 {
  clrscr();
- gotoxy(25,10);printf("Convert Miles to .......");
+ gotoxy(25,8);printf("Convert Miles to .......");
  gotoxy(20,12);printf("1.Meter...........Press 1");
  gotoxy(20,14);printf("2.Centimeter .... Press 2");
  gotoxy(20,16);printf("3.Kilometer.......Press 3");
@@ -106,16 +106,16 @@ float mile(float er)
  switch(a)
  {
   case 1:va=er*1609.0;
-	 gotoxy(20,21);printf("Mile : %.2f--------->Meter : ",er);
+	 gotoxy(20,25);printf("Mile : %.2f--------->Meter : ",er);
 	 break;
   case 2:va=er*160934.0;
-	 gotoxy(20,21);printf("Mile : %.2f--------->Centimeter : ",er);
+	 gotoxy(20,25);printf("Mile : %.2f--------->Centimeter : ",er);
 	 break;
   case 3:va=er*1.609;
-	 gotoxy(20,21);printf("Mile : %.2f--------->Kilometer : ",er);
+	 gotoxy(20,25);printf("Mile : %.2f--------->Kilometer : ",er);
 	 break;
   case 4:va=er/1.151;
-	 gotoxy(20,21);printf("Mile : %.2f--------->Nautical_Mile : ",er);
+	 gotoxy(20,25);printf("Mile : %.2f--------->Nautical_Mile : ",er);
 	 break;
  }
  return(va);
@@ -125,7 +125,7 @@ float mile(float er)
 float naumi(float er)
 {
  clrscr();
- gotoxy(25,10);printf("Convert Nautical_Miles to .......");
+ gotoxy(25,8);printf("Convert Nautical_Miles to .......");
  gotoxy(20,12);printf("1.Meter........Press 1");
  gotoxy(20,14);printf("2.Centimeter...Press 2");
  gotoxy(20,16);printf("3.Kilometer....Press 3");
@@ -136,18 +136,17 @@ float naumi(float er)
  switch(a)
  {
   case 1:va=er*1852.0;
-	 gotoxy(20,21);printf("Nautical_Mile : %.2f--------->Meter : ",er);
+	 gotoxy(20,25);printf("Nautical_Mile : %.2f--------->Meter : ",er);
 	 break;
   case 2:va=er*185200.0;
-	 gotoxy(20,21);printf("Nautical_Mile : %.2f--------->Centimeter : ",er);
+	 gotoxy(20,25);printf("Nautical_Mile : %.2f--------->Centimeter : ",er);
 	 break;
   case 3:va=er*1.852;
-	 gotoxy(20,21);printf("Nautical_Mile : %.2f--------->Kilometer : ",er);
+	 gotoxy(20,25);printf("Nautical_Mile : %.2f--------->Kilometer : ",er);
 	 break;
   case 4:va=er*1.151;
-	 gotoxy(20,21);printf("Nautical_Mile : %.2f--------->Mile : ",er);
+	 gotoxy(20,25);printf("Nautical_Mile : %.2f--------->Mile : ",er);
 	 break;
  }
  return(va);
 }
-
